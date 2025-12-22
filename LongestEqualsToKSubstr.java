6d0ea3bb5722c993320a612923e91607d16d0125
Yogeshleo8077 <yogeshleo420@gmail.com>
2025-12-22 15:35:41 +0530

    New DSA Question Added

--- LongestEqualsToKSubstr.java
+++ LongestEqualsToKSubstr.java
@@ -1,15 +1,16 @@
 import java.util.HashMap;
 
-public class LongestKSubstr {
+public class LongestEqualsToKSubstr {
     public static int longestKSubstr(String str, int k) {
         HashMap<Character, Integer> map = new HashMap<>();
-        int maxLen = 0;
+        int maxLen = -1;
         int left = 0;
 
         for (int right = 0; right < str.length(); right++) {
             char ch = str.charAt(right);
             map.put(ch, map.getOrDefault(ch, 0) + 1);
 
+            // remove characters from the left if map exceed k
             while (map.size() > k) {
                 char leftChar = str.charAt(left);
                 map.put(leftChar, map.get(leftChar) - 1);
@@ -21,16 +22,17 @@ public class LongestKSubstr {
                 left++;
             }
 
-            // update max length
-            maxLen = Math.max(maxLen, right - left + 1);
+            if (map.size() == k) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
         }
 
         return maxLen;
     }
 
     public static void main(String[] args) {
-        String str = "eceba";
-        int k = 2;
+        String str = "aabacbebebe";
+        int k = 3;
 
         System.out.println(longestKSubstr(str, k));
     }
