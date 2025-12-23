b634ead32a26a849908ee84c12a280a5b9939df0
Yogeshleo8077 <yogeshleo420@gmail.com>
2025-12-23 13:33:00 +0530

    New DSA Question Added

---
+++ ReverseStringUsingStack.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class ReverseStringUsingStack {
+    public static String reverseString(String str) {
+        if (str == null || str.isEmpty()) {
+            return "-1";
+        }
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i));
+        }
+
+        String reversed = "";
+        while (!stack.isEmpty()) {
+            reversed += stack.pop();
+        }
+
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        String str = "JAVA";
+        System.out.println(reverseString(str));
+    }
+}
